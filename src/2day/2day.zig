faae637572b707cc2d76829a406c0b6552df7e02
Krendel3 <daych1337@gmail.com>
2024-12-02 19:54:19 +0500

    d

--- src/2day/2day.zig
+++ src/2day/2day.zig
@@ -6,8 +6,6 @@ pub fn main() !void{
     var gpa =std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     allocator = gpa.allocator();
-
-    
     var file = try std.fs.cwd().openFile("src/2day/input2.txt", .{});
     defer file.close();
 
@@ -17,15 +15,11 @@ pub fn main() !void{
     var count : int = 0;
     p("{d} \n",.{while (iter.next()) |val|{
         if (val.len == 0) break count;
-        if (try isSafe(val)) {count += 1;}
-        
+        if (try isSafe(val)) {count += 1;}    
     }else count});
     
-
 }
 pub fn isSafe(val : [] const u8) !bool{
-    
-
     var list = std.ArrayList(int).init(allocator);
     defer list.deinit();
     var iter =std.mem.splitScalar(u8, val, " "[0]);
@@ -37,29 +31,13 @@ pub fn isSafe(val : [] const u8) !bool{
         var last : int = -1;
         var last_grow = true;
         var first_grow_comp = true;
-        const valid = for (0..length) |i|{
+        if(for (0..length) |i|{
             if (i == ignored) continue;
             defer last = list.items[i];
             if (last == -1) continue;
             const offset : int = list.items[i] - last;
             if (@abs(offset) < 1 or @abs(offset) > 3 or (last_grow != (offset > 0) and !first_grow_comp)) break false;
             last_grow = offset > 0; first_grow_comp = false;
-        } else true;
-        if (valid) break true;
+        } else true)break true;
     } else false;
 }
-// pub fn isSafe(iter : *std.mem.SplitIterator(u8,.scalar)) !bool{
-//     var last : int = -1;
-    
-//     var last_grow = true;
-//     var first_grow_comp = true;
-//     return while (iter.next()) |num|{
-        
-//         const number = std.fmt.parseInt(int, num, 10) catch continue;
-//         defer last = number;
-//         if (last == -1) continue;
-//         const offset : int = number - last;
-//         if (@abs(offset) < 1 or @abs(offset) > 3 or (last_grow != (offset > 0) and !first_grow_comp)) break false;
-//         last_grow = offset > 0; first_grow_comp = false;
-//     }else true;
-// }
